0531f6a9e2559a7b6c10b327434493543c407898
bwaah1 <119738911+bwaah1@users.noreply.github.com>
2023-01-23 16:30:24 +0200

    Update tasks-01.js

--- tasks-01.js
+++ tasks-01.js
@@ -19,12 +19,7 @@ function makeNegative(num) {
   //https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
   
   function greet (name, owner) {
-    if ( name === owner) {
-      return "Hello boss" ;
-    } 
-    else {
-      return "Hello guest";
-    }
+    return name === owner ? 'Hello boss' : 'Hello guest';
   }
 
   //https://www.codewars.com/kata/keep-hydrated-1/train/javascript
