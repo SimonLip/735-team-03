6a63a43f31c2b2e3cec52784a8085008cfa81fd3
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2023-01-25 18:11:10 +0200

    Update tasks-02.js

--- tasks-02.js
+++ tasks-02.js
@@ -1,27 +1,28 @@
-https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
   return string.split(" ");
 }
 
-https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
   return dna.replace(/T/g, "U");
 }
 
-https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-let min = function(list){
-    return Math.min(...list);
-}
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
-let max = function(list){
-    return Math.max(...list);
-}
+let min = function (list) {
+  return Math.min(...list);
+};
+
+let max = function (list) {
+  return Math.max(...list);
+};
 
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
-function min(arr, toReturn)
+function min(arr, toReturn) {
   let val = Math.min.apply(null, arr);
-  return toReturn == 'value' ? val : arr.indexOf(val); 
+  return toReturn == "value" ? val : arr.indexOf(val);
 }
 
 //https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
@@ -98,13 +99,12 @@ function isDivisible(n, x, y) {
   return (n % x) + (n % y) === 0;
 }
 
-https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+//https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
 function firstNonConsecutive(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i + 1] - arr[i] !== 1) {
-            return arr[i + 1];
-        }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] - arr[i] !== 1) {
+      return arr[i + 1];
     }
-    return null;
+  }
+  return null;
 }
-
