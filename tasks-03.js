3ed0be977bf17c7fca586cd77e81f1809252ae85
bwaah1 <119738911+bwaah1@users.noreply.github.com>
2023-01-26 23:14:18 +0200

    Update tasks-03.js

--- tasks-03.js
+++ tasks-03.js
@@ -1,17 +1,28 @@
 //https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 class SmallestIntegerFinder {
-  findSmallestInt = args => Math.min(...args);
+  findSmallestInt = (args) => Math.min(...args);
 }
 
 //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
-const circleCircumference = circle => 2 * Math.PI * circle.radius;
+const circleCircumference = (circle) => 2 * Math.PI * circle.radius;
 
 //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
-function giveMeFive(obj){
-  let arr = []
-  for (let i in obj){
-    if (i.length === 5)  arr.push(i)
-    if (obj[i].length === 5)  arr.push(obj[i])
+function giveMeFive(obj) {
+  let arr = [];
+  for (let i in obj) {
+    if (i.length === 5) arr.push(i);
+    if (obj[i].length === 5) arr.push(obj[i]);
   }
-  return arr
+  return arr;
+}
+
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+function buildFun(n) {
+  let res = [];
+  for (let i = 0; i < n; i++) {
+    res[i] = function () {
+      return i;
+    };
+  }
+  return res;
 }
